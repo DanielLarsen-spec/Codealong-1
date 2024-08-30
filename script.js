3a1cadfc488f7fbf66c878b6f1bec3857ead82d6
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-08-30 14:53:18 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let div = document.getElementById("setning")
+let setning = ""
+
+
+function button(text){
+setning += text + " "
+div.innerHTML = setning;   
+
+}
+
+function Reset(){
+    
+    div.innerHTML = "Her kommer det en setning"
+    setning = ""
+
+
+
+
+
+}
+
